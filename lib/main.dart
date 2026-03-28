38371a15f0327f2a5cd4aacf8af1becdbddbbd3b
fariio <fares.ahmed7474@gmail.com>
2026-03-28 21:49:38 +0200

    + Initialize Project.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Evently());
+}
+
+class Evently extends StatelessWidget{
+  const Evently({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
