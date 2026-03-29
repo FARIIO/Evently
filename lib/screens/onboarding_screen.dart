d17f46f6454fa08c474c00c101d1c97f91ce441b
fariio <fares.ahmed7474@gmail.com>
2026-03-29 23:39:16 +0200

    + Create Dark Theme Mode + Create Localization Arabic and English Language

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,11 @@
+import 'package:evently/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+
+}
